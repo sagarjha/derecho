e1cc589e5edfa9a1fff1cba71a2b461efeb784a4
Siqiu Yao <yaosiqiu@gmail.com>
2019-03-21 09:38:14 -0400

    a simple example on derecho-tensorflow interaction

---
+++ applications/archive/rdma_for_ml2.cpp
@@ -0,0 +1,259 @@
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <unistd.h>
+#include <vector>
+
+#include "derecho/derecho.h"
+
+#include "sst/poll_utils.h"
+#include "sst/sst.h"
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+const int PERMISSION = 0666; //-rw-rw-rw-
+
+
+using namespace derecho;
+using namespace sst;
+using namespace std;
+
+class MLSST : public SST<MLSST> {
+public:
+    MLSST(const std::vector<uint32_t>& members, uint32_t my_id, uint32_t dimension)
+            : SST<MLSST>(this, SSTParams{members, my_id}),
+              ml_parameters(dimension) {
+        SSTInit(ml_parameters, round);
+    }
+    SSTFieldVector<double> ml_parameters;
+    SSTField<uint64_t> round;
+};
+
+void print(const MLSST& sst) {
+    for(uint row = 0; row < sst.get_num_rows(); ++row) {
+        for(uint param = 0; param < sst.ml_parameters.size(); ++param) {
+            cout << sst.ml_parameters[row][param] << " ";
+        }
+        cout << endl;
+
+        cout << sst.round[row] << endl;
+    }
+    cout << endl;
+}
+
+void* shmalloc(size_t size, int &shmid, key_t shkey) {
+	if ((shmid = shmget(shkey, size, IPC_CREAT | PERMISSION)) == -1) {
+		perror("shmget: shmget failed");
+		exit(-1);
+	}
+	void *ret;
+   	if ((ret = shmat(shmid, NULL, 0)) == (void*)-1) {
+		perror("shmat: shmat failed");
+		exit(-1);
+	}
+	return ret;
+}
+
+void shdelete(void *maddr, int shmid) {
+	if (shmdt(maddr) == -1) {
+		perror("shmdt: shmdt failed");
+		exit(-1);
+	}
+	if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+		perror("shmctl: shmctl failed");
+		exit(-1);
+	}
+}
+
+int semalloc(key_t key, int dimension, int permission) 
+{
+    int ret = semget(key, dimension, permission);
+    if (ret < 0) {
+        perror("Cannot find semaphore, exiting.\n");
+        exit(-1);
+    }
+    return ret;
+}
+
+void semacquire(int semid)
+{
+    struct sembuf op[1];
+    op[0].sem_num = 0;
+    op[0].sem_op = -1;
+    op[0].sem_flg = 0;
+    if (semop(semid, op, 1) != 0) {
+        perror("Sem acquire failed, exiting. \n");
+        exit(-1);
+    }
+}
+
+void semrelease(int semid)
+{
+    struct sembuf op[1];
+    op[0].sem_num = 0;
+    op[0].sem_op = 1;
+    op[0].sem_flg = 0;
+    if (semop(semid, op, 1) != 0) {
+        perror("Sem release failed, exiting. \n");
+        exit(-1);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    srand(getpid());
+    std::cerr << "Derecho program starting up" << std::endl;
+
+    if(argc < 5) {
+        cout << "Usage: " << argv[0] << " <num_nodes> <num_params> <sem_shk> <sm_shk>" << endl;
+        return -1;
+    }
+
+    // the number of nodes for this test
+    const uint32_t num_nodes = std::stoi(argv[1]);
+    const uint32_t num_params = std::stoi(argv[2]);
+    const key_t sem_shk = std::stoi(argv[3]);
+    const key_t sm_shk = std::stoi(argv[4]);
+
+    uint32_t my_id = getConfUInt32(CONF_DERECHO_LOCAL_ID);
+    
+    std::cout << "Input the IP addresses" << std::endl;
+    // assume all have the same port
+//    uint16_t port = getConfUInt16(CONF_DERECHO_SST_PORT);
+    // input the ip addresses
+    std::map<uint32_t, std::pair<std::string, uint16_t>> ip_addrs_and_ports;
+    for(uint i = 0; i < num_nodes; ++i) {
+        std::string ip;
+        std::cin >> ip;
+	std::cerr << ip << std::endl;
+        uint32_t port;
+        std::cin >> port;
+        ip_addrs_and_ports[i] = {ip, port};
+    }
+
+    std::cout << my_id << endl;
+
+    // initialize the rdma resources
+#ifdef USE_VERBS_API
+    verbs_initialize(ip_addrs_and_ports, my_id);
+#else
+    lf_initialize(ip_addrs_and_ports, my_id);
+#endif
+
+    std::cerr << "init done!" << endl;
+    // form a group with a subset of all the nodes
+    std::vector<uint32_t> members(num_nodes);
+    for(unsigned int i = 0; i < num_nodes; ++i) {
+        members[i] = i;
+    }
+
+    MLSST sst(members, my_id, num_params);
+    uint32_t my_rank = sst.get_local_index();
+    // initialization
+    for(uint param = 0; param < sst.ml_parameters.size(); ++param) {
+        sst.ml_parameters[my_rank][param] = 0;
+    }
+    sst.round[my_rank] = 0;
+    sst.sync_with_members();
+
+    int semid = 0;
+    double *sm_ptr = NULL;
+    uint32_t server_rank = 0;
+    int size = num_params, shmid;
+    sm_ptr = (double*)shmalloc(sizeof(double) * size, shmid, sm_shk);
+    semid = semalloc(sem_shk, 1, PERMISSION);
+
+    if(my_rank == server_rank) {
+        std::function<bool(const MLSST&)> round_complete = [my_rank, server_rank](const MLSST& sst) {
+            for(uint row = 0; row < sst.get_num_rows(); ++row) {
+                // ignore server row
+                if(row == server_rank) {
+                    continue;
+                }
+                if(sst.round[row] == sst.round[my_rank]) {
+                    return false;
+                }
+            }
+            std::cerr << "round #" << sst.round[my_rank] << " complete" << endl;
+            return true;
+        };
+
+        std::function<void(MLSST&)> compute_average = [my_rank, server_rank](MLSST& sst) {
+            //print(sst);
+            for(uint param = 0; param < sst.ml_parameters.size(); ++param) {
+                double sum = 0;
+                for(uint row = 0; row < sst.get_num_rows(); ++row) {
+                    // ignore server row
+                    if(row == server_rank) {
+                        continue;
+                    }
+                    sum += sst.ml_parameters[row][param];
+                }
+                sst.ml_parameters[my_rank][param] = sum / (sst.get_num_rows() - 1);
+            }
+            sst.put_with_completion((char*)std::addressof(sst.ml_parameters[0][0]) - sst.getBaseAddress(), sizeof(sst.ml_parameters[0][0]) * sst.ml_parameters.size());
+            sst.round[my_rank]++;
+            sst.put_with_completion((char*)std::addressof(sst.round[0]) - sst.getBaseAddress(), sizeof(sst.round[0]));
+        };
+
+        sst.predicates.insert(round_complete, compute_average, PredicateType::RECURRENT);
+    }
+
+    else {
+        std::function<bool(const MLSST&)> server_done = [my_rank, server_rank](const MLSST& sst) {
+            return sst.round[server_rank] == sst.round[my_rank];
+        };
+
+        std::function<void(MLSST&)> compute_new_parameters = [my_rank, server_rank, semid, sm_ptr](MLSST& sst) {
+            //print(sst);
+            std::cerr << "updating new paras " << semid << " " << sm_ptr << endl;
+            for(uint param = 0; param < sst.ml_parameters.size(); ++param) {
+                sm_ptr[param] = sst.ml_parameters[server_rank][param];
+            }
+            std::cerr << " " << sst.ml_parameters.size() << " " << sst.ml_parameters[server_rank][0] << std::endl;
+            semrelease(semid);
+            semacquire(semid);
+            std::cerr << "reading new paras" << endl;
+            for(uint param = 0; param < sst.ml_parameters.size(); ++param) {
+                sst.ml_parameters[my_rank][param] = sm_ptr[param];
+                // double tmp;
+                // std::cin >> tmp;
+                // sst.ml_parameters[my_rank][param] = tmp;
+                //sst.ml_parameters[my_rank][param] = rand() % 100;
+            }
+
+            sst.put_with_completion((char*)std::addressof(sst.ml_parameters[0][0]) - sst.getBaseAddress(), sizeof(sst.ml_parameters[0][0]) * sst.ml_parameters.size());
+            sst.round[my_rank]++;
+            sst.put_with_completion((char*)std::addressof(sst.round[0]) - sst.getBaseAddress(), sizeof(sst.round[0]));
+        };
+
+        std::cerr << "Im a worker with keys: " << sm_shk << " " << sem_shk << endl;
+        // shared stuff setup
+
+        std::cerr << "releasing the lock" << endl;
+        semrelease(semid);
+        std::cerr << "acquiring the lock" <<endl;
+        semacquire(semid);
+        std::cerr << "loading new paras" << endl;
+        for(uint param = 0; param < sst.ml_parameters.size(); ++param) {
+            sst.ml_parameters[my_rank][param] = sm_ptr[param];
+            // double tmp;
+            // std::cin >> tmp;
+            // sst.ml_parameters[my_rank][param] = tmp;
+            //sst.ml_parameters[my_rank][param] = rand() % 100;
+        }
+
+        sst.put_with_completion((char*)std::addressof(sst.ml_parameters[0][0]) - sst.getBaseAddress(), sizeof(sst.ml_parameters[0][0]) * sst.ml_parameters.size());
+        sst.round[my_rank]++;
+        sst.put_with_completion((char*)std::addressof(sst.round[0]) - sst.getBaseAddress(), sizeof(sst.round[0]));
+        sst.predicates.insert(server_done, compute_new_parameters, PredicateType::RECURRENT);
+    }
+
+    while(true) {
+    }
+}
---
+++ applications/archive/simplenn.py
@@ -0,0 +1,183 @@
+""" Neural Network.
+A 2-Hidden Layers Fully Connected Neural Network (a.k.a Multilayer Perceptron)
+implementation with TensorFlow. This example is using the MNIST database
+of handwritten digits (http://yann.lecun.com/exdb/mnist/).
+Links:
+    [MNIST Dataset](http://yann.lecun.com/exdb/mnist/).
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+"""
+
+from __future__ import print_function
+import subprocess
+import io
+import sysv_ipc
+import array
+import struct
+derecho_program = "../rdma_for_ml2"
+derecho_numnode = 2
+
+# shared semaphore and memory
+semshk = 314158
+smshk = 314259
+permission = 0o666
+size = None
+type_size = 8 # double
+byte_size = None
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+import tensorflow as tf
+
+# Parameters
+learning_rate = 0.1
+num_steps = 1000
+batch_size = 128
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_2 = 256 # 2nd layer number of neurons
+num_input = 784 # MNIST data input (img shape: 28*28)
+num_classes = 10 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+X = tf.placeholder("float", [None, num_input])
+Y = tf.placeholder("float", [None, num_classes])
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+
+
+# Create model
+def neural_net(x):
+    # Hidden fully connected layer with 256 neurons
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    # Hidden fully connected layer with 256 neurons
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    # Output fully connected layer with a neuron for each class
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Construct model
+logits = neural_net(X)
+prediction = tf.nn.softmax(logits)
+
+# Define loss and optimizer
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss_op)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+V = tf.trainable_variables()
+D = [v.get_shape() for v in V]
+SIZE = sum([d.num_elements() for d in D])
+
+# shared memory
+size = SIZE
+byte_size = size * type_size
+sem = sysv_ipc.Semaphore(semshk, flags = sysv_ipc.IPC_CREAT, mode = permission, initial_value = 0)
+mem = sysv_ipc.SharedMemory(smshk, flags=sysv_ipc.IPC_CREAT, mode = permission, size = byte_size)
+
+derecho = subprocess.Popen([derecho_program, str(derecho_numnode), str(SIZE), str(semshk), str(smshk)], stdin=subprocess.PIPE, stdout = subprocess.PIPE)
+message = derecho.stdout.readline()
+stdin_wrapper = io.TextIOWrapper(derecho.stdin, 'utf-8')
+stdin_wrapper.write("127.0.0.1\n")
+stdin_wrapper.write("37683\n")
+stdin_wrapper.write("127.0.0.1\n")
+stdin_wrapper.write("37684\n")
+stdin_wrapper.flush()
+message = derecho.stdout.readline()
+if int(message.decode("utf-8").strip()) == 0 :
+    print("server", SIZE)
+    while True :
+        continue
+
+sem.acquire()
+
+# Start training
+with tf.Session() as sess:
+
+    # Run the initializer
+    sess.run(init)
+    print("Total Steps: " + str(num_steps))
+
+    for step in range(1, num_steps+1):
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        # Run optimization op (backprop)
+        sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+        if step % display_step == 0 or step == 1:
+            # Calculate batch loss and accuracy
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
+                                                                 Y: batch_y})
+            print("Step " + str(step) + ", Minibatch Loss= " + \
+                  "{:.4f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.3f}".format(acc))
+
+            V = tf.trainable_variables()
+            D = [(v.name, v.value()) for v in V]
+            arrays = [d[1].eval(session=sess) for d in D]
+
+            #print(arrays)
+            sps = [arr.shape for arr in arrays]
+            szs = [arr.size for arr in arrays]
+
+            #print(len(szs), szs)
+            idxs = [szs[0]]
+            for sz in szs[1:-1] :
+                idxs.append(idxs[-1] + sz)
+            #print(idxs)
+
+            lst = [arr.reshape([1, -1]) for arr in arrays]
+            import numpy
+            tmp = numpy.concatenate((lst), axis = 1).ravel().tolist()
+            mem.write(struct.pack("%sd" % len(tmp), *tmp))
+            sem.release()
+            print("releasing the lock")
+            sem.acquire()
+            print("acquired the lock")
+            #for ele in tmp :
+            #    #print("writing")
+            #    stdin_wrapper.write(str(ele) + " ")
+            #    stdin_wrapper.flush()
+            # tmp = ' '.join(map(str, tmp)) + "\n"
+            #stdin_wrapper.write(tmp)
+            #stdin_wrapper.write("\n")
+            #stdin_wrapper.flush()
+
+
+            newparas = array.array("d", mem.read());
+            #newparas = derecho.stdout.readline().decode("utf-8")
+            print(newparas, len(newparas))
+            print("roundgood")
+            #newparas = numpy.fromstring(newparas)
+            newparas = numpy.frombuffer(newparas)
+
+            rec = numpy.split(newparas, idxs)
+            recs = [rec[i].reshape(sps[i]) for i in range(len(rec))]
+            for i in range(len(V)) :
+                V[i].load(recs[i], sess)
+
+    print("Optimization Finished!")
+
+    # Calculate accuracy for MNIST test images
+    print("Testing Accuracy:", \
+        sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
