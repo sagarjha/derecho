5e0245805c1a4b561f53eac5266820f3a8a81786
Sagar Jha <sjsagarjha3@gmail.com>
2019-05-15 16:58:07 -0400

    Bug fix in the script

--- applications/archive/run_rdma_async.sh
+++ applications/archive/run_rdma_async.sh
@@ -3,7 +3,7 @@
 echo Server or worker argument is $1
 echo Number of nodes is $2
 
-cmd="python3 ~/derecho/applications/archive/setup.py --$1 --num-nodes $2 --python-path ~/derecho/applications/archive/train.py --derecho-path ./rdma_for_ml2_async"
+cmd="python3 /home/rdma_for_ml/derecho/applications/archive/setup.py --$1 --num-nodes $2 --python-path /home/rdma_for_ml/derecho/applications/archive/train.py --derecho-path ./rdma_for_ml2_async"
 
 echo Running $cmd
 
